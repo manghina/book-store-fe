6bd6c021a4c332ba9cb50f4d9f58b0e72dc10d41
CodeSt@r <joshephan422@gmail.com>
2024-06-23 02:19:57 +0900

    axios inspector

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,19 +1,14 @@
-import { baseURL } from '../config/baseUrl';
-import axios from 'axios';
-
-const apiClient = await axios.create({
-  baseURL: baseURL,
-});
-
+import axios from './apiClient';
 
 export default {
   login(email, password) {
-    return apiClient.post(baseURL + '/login', {email, password});
+    return axios.post('/login', {email, password});
   },
 
   // localStorage.clear()
   logout(data) {
-    return apiClient.post('/api/logout', data);
+    localStorage.removeItem('user');
+    return axios.post('/api/logout', data);
   },
 
 };
