90707927d7cebdce1b5eaaceb41f8c662b8386dc
CODE_ST@R <joshephan422@gmail.com>
2024-06-24 20:57:34 +0900

    remove conflict

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,20 +1,13 @@
 import apiClient from './apiClient';
 
 export default {
-<<<<<<< HEAD
   login(email, password, router) {
     return apiClient.post('/login', {email, password}).then(res => {
       const token = res.data.token; // Assuming the server sends back a token
       localStorage.setItem("token", token); // Store token in localStorage
       router.push('/')
     });
-=======
-  async login(email, password) {
-    return axios.post('/login', {email, password});
->>>>>>> 7364d9715d2a73ba3e3443a3e756de7d27316c01
   },
-
-  // localStorage.clear()
   logout(data) {
     localStorage.removeItem('token');
     return apiClient.post('/api/logout', data);
--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import apiClient from './apiClient';
-=======
-import axios from './apiClient';
-const route = '/book'
->>>>>>> 7364d9715d2a73ba3e3443a3e756de7d27316c01
 
 export default {
   
   getBooks() {
-<<<<<<< HEAD
     return apiClient.get('/books');
   },
   addbook(data) {
@@ -23,26 +17,5 @@ export default {
 
   deletebook(id) {
     return apiClient.delete(`/api/book/${id}`);
-  },
-=======
-    return axios.get('/books');
-  },
-
-  getBook(id) {
-    return axios.get('/book/' + id);
-  },
-
-  create(data) {
-    return axios.put('/api' + route, data);
-  },
-
-  update(data) {
-    return axios.post('/api' + route, data);
-  },
-
-  delete(data) {
-    return axios.delete('/api' + route + '/' + data.id);
   }
-
->>>>>>> 7364d9715d2a73ba3e3443a3e756de7d27316c01
 };
